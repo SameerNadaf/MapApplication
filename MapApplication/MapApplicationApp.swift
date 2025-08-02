36276434d34f1701b5f6be4d8964d8800adb5110
Sameer Nadaf <nsameernadaf@gmail.com>
2025-08-02 17:25:49 +0530

    Initial Commit

---
+++ MapApplication/MapApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApplicationApp.swift
+//  MapApplication
+//
+//  Created by Sameer  on 02/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MapApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
